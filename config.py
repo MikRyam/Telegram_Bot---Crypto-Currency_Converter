e99bb93e2b00e91a06bb2ec2b0675903dea6e2f6
MikRyam <mikhailryamov@gmail.com>
2021-09-15 15:28:27 +0300

    Telegram Bot - Crypto & Currency Converter.

---
+++ config.py
@@ -0,0 +1,10 @@
+
+TOKEN = '1951943626:AAFx_Iq7rri-KfXlZN86gt1fHUZdM8l-830'
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+}
